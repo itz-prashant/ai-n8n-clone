11f35343b323b0eeeb06fc8beeeecf6cbba8b4d8
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-04-29 11:00:03 +0530

    feat :setup trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import prisma from "@/lib/db";
+import { caller } from "@/trps/server";
 
 async function Home() {
-  const users = await prisma.user.findMany()
+  const users = await caller.getUsers()
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       {JSON.stringify(users)}
